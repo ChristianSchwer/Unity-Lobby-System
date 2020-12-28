06ae1e29624e2355aee09e39d42d2134920bc4f5
Christian Schwer <christianschwer00@gmail.com>
2020-12-28 19:52:55 +0100

    change branch

---
+++ Assets/Scripts/CreateRoom.cs
@@ -0,0 +1,71 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviourPunCallbacks
+{
+    #region Private Fields
+
+    [SerializeField]
+    private Text _roomName;
+    [SerializeField]
+    private InputField _numberOfPlayer;
+
+    private RoomsCanvases _roomsCanvases;
+
+    #endregion
+
+    #region Public Fields
+
+    int maxPlayersPerRoom = 4;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        //maxPlayersPerRoom = Convert.ToInt32(_numberOfPlayer.text);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        Debug.Log("NetworkManager: CreateRoom()");
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = Convert.ToByte(maxPlayersPerRoom);
+        PhotonNetwork.CreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+    public void FirstInitialize(RoomsCanvases canvases)
+    {
+        _roomsCanvases = canvases;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("NetworkManager: OnCreatedRoom() Room successfully created.", this);
+        _roomsCanvases.CurrentRoomCanvas.Show();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("NetworkManager: OnCreatedRoomFailed() Room creation failed: " + message, this);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    #region Private Fields
+
+    [SerializeField]
+    private string gameVersion = "1";
+    [SerializeField]
+    private GameObject controlPanel;
+    [SerializeField]
+    private GameObject progressLabel;
+
+    #endregion
+
+    #region Public Fields
+
+    public InputField nickName;
+    public SwitchScene switchScene;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect()
+    {
+        PhotonNetwork.NickName = nickName.text;
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+        else
+        {
+            Debug.Log("NetworkManager: Connect() wird ausgeführt.");
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("NetworkManager: OnConnectedToMaster() was called by PUN.");
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("NetworkManager: OnJoinedLobby() mit Lobby verbunden.");
+        switchScene.NextScene("LobbyScene");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("NetworkManager: OnDisconnected() Reason " + cause.ToString());
+    }
+
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+}
